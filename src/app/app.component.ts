27c148479d7ae920f0add5da2af54b7fe5f258ae
Wineo <mreza.jahankhah@gmail.com>
2020-03-23 12:17:50 +0430

    prepare for farapar#1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'farapar-app';
-  name = 'hello';
-  cal = 23;
-  age = 33;
+  title = 'farapar';
 }
