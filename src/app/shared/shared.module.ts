cf426a8cbb4e2051b0d6cc55fe288cdd786d2178
wineo <mreza.jahankhah@gmail.com>
2020-06-06 00:53:25 +0430

    upgrade font version

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from '../app-routing.module';
 import {FeaturedInputComponent} from './featured-input/featured-input.component';
 import {TableViewComponent} from './table-view/table-view.component';
 import {FeaturedModalComponent} from './featured-modal/featured-modal.component';
+import { PhoneInputComponent } from './phone-input/phone-input.component';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import {FeaturedModalComponent} from './featured-modal/featured-modal.component'
     VerticalListComponent,
     FeaturedInputComponent,
     TableViewComponent,
-    FeaturedModalComponent],
+    FeaturedModalComponent,
+    PhoneInputComponent],
   imports: [
     CommonModule,
     AppRoutingModule
