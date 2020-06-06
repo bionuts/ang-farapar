7b63dba08ea110ea35363b8705a8f1c32e409bb4
wineo <mreza.jahankhah@gmail.com>
2020-06-06 14:56:06 +0430

    next step of login modal page

--- src/app/shared/steps-login/steps-login.component.ts
+++ src/app/shared/steps-login/steps-login.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepsLoginComponent implements OnInit {
 
+  loginByPhone = true;
+  loginByEmail = false;
+  maintitle = 'ورود / ثبت نام به فراپر';
+  subtitle = 'شماره موبایل';
+
+  switchLinkName = 'آدرس ایمیل';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  NextStep() {
+    return true;
+  }
+
+  LoginByEmail() {
+    if (this.loginByPhone === true) {
+      this.loginByPhone = false;
+      this.maintitle = 'ورود با ایمیل';
+      this.subtitle = 'آدرس ایمیل';
+      this.switchLinkName = 'شماره موبایل';
+      this.loginByEmail = true;
+    } else if (this.loginByEmail === true) {
+      this.loginByEmail = false;
+      this.maintitle = 'ورود / ثبت نام به فراپر';
+      this.subtitle = 'شماره موبایل';
+      this.switchLinkName = 'آدرس ایمیل';
+      this.loginByPhone = true;
+    }
   }
 
 }
