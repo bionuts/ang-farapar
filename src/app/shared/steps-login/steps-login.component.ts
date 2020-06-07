5677fc5f9dcfae8a662267dc5c17cc62001bb652
wineo <mreza.jahankhah@gmail.com>
2020-06-07 09:35:47 +0430

    login flow#1

--- src/app/shared/steps-login/steps-login.component.ts
+++ src/app/shared/steps-login/steps-login.component.ts
@@ -7,16 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepsLoginComponent implements OnInit {
 
-  firstStep: boolean;
+  firstStep: boolean; // login first page ( mobile / email and confirmed sms or email )
+  secondStep: boolean; // confirm page
+  thirdStep: boolean; // login with mobile/email and password
+
   loginByEmail: boolean;
   loginByPhone: boolean;
 
 
-  secondStep: boolean;
   mobile: string = '09392474188';
   mainTitle = 'ورود / ثبت&zwnj;نام به فراپر';
   subTitle = 'لطفا برای ادامه، شماره موبایل خود را وارد کنید';
-  label1 = 'شماره موبایل';
   switchLinkName = 'آدرس ایمیل';
 
   constructor() {
@@ -24,18 +25,21 @@ export class StepsLoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.firstStep = true;
+    this.secondStep = false;
+    this.thirdStep = false;
     this.loginByPhone = true;
     this.loginByEmail = false;
-    this.secondStep = false;
   }
 
   NextStep() {
-    this.firstStep = false;
-    this.secondStep = true;
+    if (this.firstStep === true) {
+      if (this.loginByPhone === true) {
+        this.firstStep = false;
+        this.secondStep = true;
 
-    if (this.loginByPhone === true) {
-      this.mainTitle = 'تائید شماره موبایل';
-      this.subTitle = 'کد ۵ رقمی پیامک شده به شماره زیر را وارد کنید';
+        this.mainTitle = 'تائید شماره موبایل';
+        this.subTitle = 'کد ۵ رقمی پیامک شده به شماره زیر را وارد کنید';
+      }
     }
   }
 
@@ -43,17 +47,33 @@ export class StepsLoginComponent implements OnInit {
     if (this.loginByPhone === true) {
       this.loginByPhone = false;
       this.mainTitle = 'ورود با ایمیل';
-      this.label1 = 'آدرس ایمیل';
       this.subTitle = 'لطفاً برای ادامه، آدرس ایمیل خود را وارد کنید';
       this.switchLinkName = 'شماره موبایل';
       this.loginByEmail = true;
     } else if (this.loginByEmail === true) {
       this.loginByEmail = false;
-      this.label1 = 'شماره موبایل';
       this.mainTitle = 'ورود / ثبت نام به فراپر';
       this.subTitle = 'لطفاً برای ادامه، شماره موبایل خود را وارد کنید';
       this.switchLinkName = 'آدرس ایمیل';
       this.loginByPhone = true;
+    }
+  }
+
+  LoginByMobileAndPass() {
+    if (this.secondStep === true && this.loginByPhone === true) {
+      this.secondStep = false;
+      this.thirdStep = true;
+      this.mainTitle = 'ورود به فراپر';
+      this.subTitle = 'لطفاً برای ادامه، رمز عبور خود را وارد کنید';
+    }
+  }
+
+  LoginByEmailAndPass() {
+    if (this.secondStep === true && this.loginByEmail === true) {
+      this.secondStep = false;
+      this.thirdStep = true;
+      this.mainTitle = 'ورود به فراپر';
+      this.subTitle = 'لطفاً برای ادامه، رمز عبور خود را وارد کنید';
     }
   }
 
