c0cc86e3bb75a388491a0c6804f8f0f284ec592e
wineo <mreza.jahankhah@gmail.com>
2020-06-14 21:06:00 +0430

    module admin for farapar team

---
+++ src/app/mod-admin/mod-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModAdminRoutingModule } from './mod-admin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModAdminRoutingModule
+  ]
+})
+export class ModAdminModule { }
