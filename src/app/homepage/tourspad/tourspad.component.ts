af828b42eb37ab45de05d19af3c84186b3db7330
wineo <mreza.jahankhah@gmail.com>
2020-06-24 14:38:55 +0430

    some change in ticket panel and log and slick slider

--- src/app/homepage/tourspad/tourspad.component.ts
+++ src/app/homepage/tourspad/tourspad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tourspad',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TourspadComponent implements OnInit {
 
-  constructor() { }
+  isDown = false;
+  startX: number;
+  scrollLeft: number;
+
+  constructor(
+    private el: ElementRef
+  ) { }
 
   ngOnInit(): void {
   }
 
+  mouseLeaveHandler(e) {
+    this.isDown = false;
+    // slider.classList.remove("active");
+    // this.el.nativeElement.classList.remove("active");
+  }
+
+  mouseDownHandler(e) {
+    if (e.which === 1) {
+      this.isDown = true;
+      // slider.classList.add("active");
+      // this.startX = e.pageX - slider.offsetLeft;
+      this.startX = e.pageX - this.el.nativeElement.offsetLeft;
+      this.scrollLeft = this.el.nativeElement.scrollLeft;
+      alert(this.startX);
+    }
+  }
+
+  mouseUpHandler(e) {
+    this.isDown = false;
+    // slider.classList.remove("active");
+  }
+
+  mouseMoveHandler(e) {
+    if (!this.isDown) { return; }
+    e.preventDefault();
+    const x = e.pageX - this.el.nativeElement.offsetLeft;
+    const walk = x - this.startX;
+    this.el.nativeElement.scrollLeft = this.scrollLeft - walk;
+  }
 }
