6778d8493d0b73fd15b4b53ae30547f6b246281c
wineo <mreza.jahankhah@gmail.com>
2020-06-27 15:57:28 +0430

    get close to real slider

--- src/app/homepage/tourspad/tourspad.component.ts
+++ src/app/homepage/tourspad/tourspad.component.ts
@@ -9,22 +9,28 @@ export class TourspadComponent implements OnInit {
 
   @ViewChild('scrollHope') myHope: ElementRef;
 
+  itemWidth = 250;
   cardCount = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   isDown = false;
   startX: number;
   scrollLeft: number;
 
+  tstartX: number;
+  twalk: number;
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.itemWidth = this.myHope.nativeElement.querySelector('.witem').offsetWidth;
   }
 
-  mouseLeaveHandler(e) {
+  mouseLeaveHandler() {
     this.isDown = false;
   }
 
   mouseDownHandler(e) {
+    this.itemWidth = this.myHope.nativeElement.querySelector('.witem').offsetWidth;
     if (e.which === 1) {
       this.isDown = true;
       this.startX = e.pageX - this.myHope.nativeElement.offsetLeft;
@@ -32,7 +38,7 @@ export class TourspadComponent implements OnInit {
     }
   }
 
-  mouseUpHandler(e) {
+  mouseUpHandler() {
     this.isDown = false;
   }
 
@@ -59,4 +65,29 @@ export class TourspadComponent implements OnInit {
       behavior: 'smooth'
     });
   }
+
+  touchStart(e) {
+    // e.targetTouches.length
+    this.itemWidth = this.myHope.nativeElement.querySelector('.witem').offsetWidth;
+    this.tstartX = e.targetTouches[0].pageX;
+  }
+
+  touchEnd(e) {
+    const trigger = this.itemWidth / 2;
+    if (this.twalk > trigger) {
+      alert(this.twalk + ':' + trigger);
+    } else {
+      this.myHope.nativeElement.scrollTo({
+        left: this.myHope.nativeElement.scrollLeft + this.twalk,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  touchMove(e) {
+    const x = e.targetTouches[0].pageX;
+    this.twalk = x - this.tstartX;
+    console.log('dif: ' + (x - this.tstartX));
+  }
+
 }
